d11331e21eee9a6138cf421db5239ae248dcc0f0
tom <93163074+tom-the-magnus@users.noreply.github.com>
2024-01-28 15:44:34 -0500

    Remove socials:

--- src/data/user.js
+++ src/data/user.js
@@ -6,14 +6,14 @@ const INFO = {
 		logo: "../logo.png",
 	},
 
-	socials: {
-		// twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
-	},
+	// socials: {
+	// 	// twitter: "https://twitter.com/",
+	// 	github: "https://github.com/",
+	// 	linkedin: "https://linkedin.com/",
+	// 	instagram: "https://instagram.com/",
+	// 	stackoverflow: "https://stackoverflow.com/",
+	// 	facebook: "https://facebook.com/",
+	// },
 
 	homepage: {
 		title: "Programmer, writer, general question-asker.",
