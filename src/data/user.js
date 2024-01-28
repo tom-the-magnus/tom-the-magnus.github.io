42db5763cb95bb444cca47cef84aa072e47262ae
tom <93163074+tom-the-magnus@users.noreply.github.com>
2024-01-27 22:28:44 -0500

    Updated all pages

--- src/data/user.js
+++ src/data/user.js
@@ -16,21 +16,21 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "This is another change just to see what happens really.",
+		title: "Programmer, writer, general question-asker.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a seasoned client-facing professional with growing abilites in Python, SQL, and Javascript.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Thomas M. I live in New York.",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+			"I've worked in sales at Salesforce and technical support at Teachable. These experiences have taught me how to quickly build rapport with customers, understand their business needs, and design solutions to fit these needs. I started learning programming because of my general interest in technology, my desire to have a more technical career, and my desire to design specific solutions to needs in my life by designing websites, writing bash scripts, or building mobile applicatons.",
 	},
 
 	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		title: "I'm passionate about technology and solving complex business problems.",
 		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+			"This is a collection of thoughts on technology, leadership, product design, and more.",
 	},
 
 	projects: [
