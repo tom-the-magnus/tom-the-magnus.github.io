287d0f8dc44497dec8f213cb33eb15620a90c76a
tom <93163074+tom-the-magnus@users.noreply.github.com>
2024-01-28 23:55:50 -0500

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -18,7 +18,7 @@ const INFO = {
 	homepage: {
 		title: "Communicator, tinkerer, general inquisitor.",
 		description:
-			"I am a seasoned client-facing professional with growing abilites in Python, SQL, and Javascript. This website hosts my projects, writings, and gives people an idea of who I am. The site is based off a react template by truethari's, is hosted on Github, and is deployed on Vercel.",
+			"I am a seasoned client-facing professional with growing abilites in Python, SQL, and Javascript. This website presents my projects and writings, and also gives people an idea of who I am. It's hosted on Github, deployed on Vercel.",
 	},
 
 	about: {
