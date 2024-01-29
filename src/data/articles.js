bc81f2d4084c37f13122fdd51172241c07d1d0d2
tom <93163074+tom-the-magnus@users.noreply.github.com>
2024-01-28 19:00:46 -0500

    Update articles.js

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,15 +2,14 @@ import React from "react";
 
 function article_1() {
 	return {
-		date: "7 May 2023",
-		title: "The Benefits of Cloud Computing",
+		date: "28 Jan 2023",
+		title: "The Purpose of This Website",
 		description:
-			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
+			"A short reflection on my intentions for this website.",
 		keywords: [
 			"The Benefits of Cloud Computing",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
+			"Tom Magnus",
+			"Tom",
 		],
 		style: `
 				.article-content {
@@ -27,38 +26,40 @@ function article_1() {
 		body: (
 			<React.Fragment>
 				<div className="article-content">
-					<div className="paragraph">Content of article 1</div>
+					<div className="paragraph">This is just a test to see what happens.</div>
 					<img
 						src="https://picsum.photos/200/300"
 						alt="random"
 						className="randImage"
 					/>
+					<div className="paragraph">So here is another paragraph.</div>
+					<div className="paragraph">So here is another paragraph.</div>
 				</div>
 			</React.Fragment>
 		),
 	};
 }
 
-function article_2() {
-	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
-		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-		style: ``,
-		keywords: [
-			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
-		],
-		body: (
-			<React.Fragment>
-				<h1>Content of article 2</h1>
-			</React.Fragment>
-		),
-	};
-}
+// function article_2() {
+// 	return {
+// 		date: "7 May 2023",
+// 		title: "Artificial Intelligence in Healthcare",
+// 		description:
+// 			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+// 		style: ``,
+// 		keywords: [
+// 			"Artificial Intelligence in Healthcare",
+// 			"Tharindu",
+// 			"Tharindu N",
+// 			"Tharindu Nayanajith",
+// 		],
+// 		body: (
+// 			<React.Fragment>
+// 				<h1>Content of article 2</h1>
+// 			</React.Fragment>
+// 		),
+// 	};
+// }
 
 const myArticles = [article_1, article_2];
 
